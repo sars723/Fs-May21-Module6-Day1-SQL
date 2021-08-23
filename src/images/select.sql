fe4448f149a7821385da91ba1f847e0b6918a273
sars723 <sarasalomonn@gmail.com>
2021-08-23 16:58:34 +0200

    Update select.sql

--- src/images/select.sql
+++ src/images/select.sql
@@ -6,9 +6,11 @@ SELECT  category FROM public.books
 /* Select books which start with a letter (any letter) */
 SELECT  * FROM public.books  WHERE name LIKE 'T%'
 
-/* Select books which include a given word  (any word)
-
- */
+/* Select books which include a given word  (any word)*/
  SELECT  * FROM public.books  WHERE name LIKE '%Murder%'
+
+/*  Select authors which are “not” older than 60 years */
+SELECT * FROM public.authors WHERE birth_year>60;
+
 /* Select authors and order them by birth_year desc */
 SELECT * FROM public.authors ORDER BY birth_year ASC;
