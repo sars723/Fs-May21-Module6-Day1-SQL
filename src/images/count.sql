8bfa1a18c986c0b843a70973258bbc8196df6f8f
sars723 <sarasalomonn@gmail.com>
2021-08-23 16:55:07 +0200

    comment

--- src/images/count.sql
+++ src/images/count.sql
@@ -1,5 +1,3 @@
 /* Count all books */
 SELECT COUNT (*) FROM public.books
 
-/* Select authors and order them by birth_year desc */
-SELECT * FROM public.authors ORDER BY birth_year ASC;
--- src/images/select.sql
+++ src/images/select.sql
@@ -1,3 +1,14 @@
+/* Select books distinct by category */
 SELECT DISTINCT category FROM public.books 
+/* Select books from a category (any) */
 SELECT  category FROM public.books
+
+/* Select books which start with a letter (any letter) */
 SELECT  * FROM public.books  WHERE name LIKE 'T%'
+
+/* Select books which include a given word  (any word)
+
+ */
+ SELECT  * FROM public.books  WHERE name LIKE '%Murder%'
+/* Select authors and order them by birth_year desc */
+SELECT * FROM public.authors ORDER BY birth_year ASC;
